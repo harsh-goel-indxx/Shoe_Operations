42fc9e4d65e1f26c95765791acf813c4ef5c9f96
Harsh Goel <hgoel@gmail.com>
2026-06-07 21:27:05 +0530

    Fix: Bypass Security

--- Operations/settings.py
+++ Operations/settings.py
@@ -97,7 +97,12 @@ DATABASES = {
     }
 }
 
-
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ],
+}
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
